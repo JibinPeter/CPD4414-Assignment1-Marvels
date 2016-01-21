eaeb390c40b10aa4fc54f6ff8c6a9f570d7ef062
c0660563 <c0660563@SB-N208-15.lambton.on.ca>
2016-01-21 11:28:36 -0500

    Created strringPower skeleton

--- src/cpd4414/assignment1/marvels/CPD4414Assignment1Marvels.java
+++ src/cpd4414/assignment1/marvels/CPD4414Assignment1Marvels.java
@@ -18,4 +18,8 @@ public class CPD4414Assignment1Marvels {
         // TODO code application logic here
     }
     
+    public static String stringPower(String str, int num) {
+        return null;
+    }
+    
 }
