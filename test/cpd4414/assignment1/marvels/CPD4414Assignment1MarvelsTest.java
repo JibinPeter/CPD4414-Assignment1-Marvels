126ff7806e46d611a2a0f3e4707ca12fe7f88b98
c0662366 <c0662366@SB-N208-16.lambton.on.ca>
2016-01-21 11:41:58 -0500

    Created testEmptyStringShouldReturnEmptyString method

---
+++ test/cpd4414/assignment1/marvels/CPD4414Assignment1MarvelsTest.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cpd4414.assignment1.marvels;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author c0662366
+ */
+public class CPD4414Assignment1MarvelsTest {
+    
+    public CPD4414Assignment1MarvelsTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class CPD4414Assignment1Marvels.
+     */
+//    @Test
+//    public void testMain() {
+//        System.out.println("main");
+//        String[] args = null;
+//        CPD4414Assignment1Marvels.main(args);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+
+    /**
+     * Test of stringPower method, of class CPD4414Assignment1Marvels.
+     */
+//    @Test
+//    public void testStringPower() {
+//        System.out.println("stringPower");
+//        String str = "";
+//        int num = 0;
+//        String expResult = "";
+//        String result = CPD4414Assignment1Marvels.stringPower(str, num);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+    @Test 
+    public void testEmptyStringShouldReturnEmptyString() {
+        String str = "";
+        int num = 0;
+        String expResult = "";
+        String result = CPD4414Assignment1Marvels.stringPower(str, num);
+        assertEquals(expResult, result);
+    }
+    
+}
